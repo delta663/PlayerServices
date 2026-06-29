3ea39cb6431f536e20ed8e19de7c782fb419c742
delta663 <dekdel@gmail.com>
2026-06-29 19:40:41 +0700

    Update ConnectionPatch.cs

--- Patches/ConnectionPatch.cs
+++ Patches/ConnectionPatch.cs
@@ -5,13 +5,14 @@ using ProjectM;
 using ProjectM.Network;
 using Stunlock.Network;
 using PlayerServices.Services;
+using PlayerServices.Data;
 using Stunlock.Core;
 using Unity.Collections;
 
 namespace PlayerServices.Patches;
 
 [HarmonyPatch(typeof(ServerBootstrapSystem), nameof(ServerBootstrapSystem.OnUserConnected))]
-public static class PlayerConnectionPatch     // Credit Bloodcraft
+public static class OnUserConnected_Patch     // Credit Bloodcraft and KindredCommands
 {
     [HarmonyPostfix]
     public static void Postfix(ServerBootstrapSystem __instance, NetConnectionId netConnectionId)
@@ -26,7 +27,7 @@ public static class PlayerConnectionPatch     // Credit Bloodcraft
             Entity userEntity = serverClient.UserEntity;
             User user = __instance.EntityManager.GetComponentData<User>(userEntity);
 
-            BlacklistService.CheckUserLogin(userEntity, user);
+            AccessControlService.CheckUserLogin(userEntity, user);
             WelcomeService.SendWelcomeMessageDelayed(userEntity, user);
             AuraService.RefreshOnConnect(userEntity, user);
         }
@@ -38,11 +39,40 @@ public static class PlayerConnectionPatch     // Credit Bloodcraft
     }
 }
 
-[HarmonyPatch(typeof(Destroy_TravelBuffSystem), nameof(Destroy_TravelBuffSystem.OnUpdate))]
-public static class PlayerCreationPatch     // Credit KindredCommands
-{
-    private static readonly PrefabGUID CoffinSpawnBuff = new PrefabGUID(722466953);
 
+[HarmonyPatch(typeof(ServerBootstrapSystem), nameof(ServerBootstrapSystem.OnUserDisconnected))]
+public static class OnUserDisconnected_Patch     // Credit Bloodcraft and KindredCommands
+{
+    [HarmonyPrefix]
+	private static void Prefix(ServerBootstrapSystem __instance, NetConnectionId netConnectionId)
+	{
+		try
+		{
+			if (!__instance._NetEndPointToApprovedUserIndex.TryGetValue(netConnectionId, out int userIndex))
+				return;
+
+			ServerBootstrapSystem.ServerClient serverClient = __instance._ApprovedUsersLookup[userIndex];
+			Entity userEntity = serverClient.UserEntity;
+
+			if (userEntity == Entity.Null ||
+				!__instance.EntityManager.Exists(userEntity) ||
+				!__instance.EntityManager.HasComponent<User>(userEntity))
+				return;
+
+			User user = __instance.EntityManager.GetComponentData<User>(userEntity);
+
+		    PlayerDataService.RecordDisconnectedUser(user);
+		}
+		catch (Exception e)
+		{
+			Core.LogException(e);
+		}
+	}
+}
+
+[HarmonyPatch(typeof(Destroy_TravelBuffSystem), nameof(Destroy_TravelBuffSystem.OnUpdate))]
+public static class Destroy_TravelBuffSystem_Patch     // Credit KindredCommands
+{
     [HarmonyPrefix]
     public static void Prefix(Destroy_TravelBuffSystem __instance)
     {
@@ -66,7 +96,7 @@ public static class PlayerCreationPatch     // Credit KindredCommands
                 var entity = entities[i];
                 var guid = em.GetComponentData<PrefabGUID>(entity);
 
-                if (guid != CoffinSpawnBuff) continue;
+                if (guid != PrefabData.TombCoffinSpawn) continue;
 
                 var owner = em.GetComponentData<EntityOwner>(entity).Owner;
                 if (!em.HasComponent<PlayerCharacter>(owner)) continue;
@@ -77,7 +107,7 @@ public static class PlayerCreationPatch     // Credit KindredCommands
 
                 string playerName = user.CharacterName.ToString();
 
-                Core.Log.LogInfo($"[Connection] Player created: {playerName} ({user.PlatformId})");
+                Core.Log.LogInfo($"[Created] Player created: {playerName} ({user.PlatformId})");
 
                 StarterKitService.GiveKit(userEntity, user);
             }
